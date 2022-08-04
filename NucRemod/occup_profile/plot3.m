e15803c436ea92a4c9bf7b276c810081be6b6b6e
hungyok <punkienutts@gmail.com>
2022-08-05 00:42:05 +0200

    Update plot3.m

--- NucRemod/occup_profile/plot3.m
+++ NucRemod/occup_profile/plot3.m
@@ -1,10 +1,10 @@
 clear;
-load D:\Cell_protocol\ndr_call\yy3A_lee.mat;
+load /nuctf_equi_bai/ndr_call/yy3A_lee.mat;
 xlee=x1_lee{1};
 ylee=y1_lee{1};
-load D:\Cell_protocol\NucTF\occup_profile\example2\O_good.mat;
+load /nuctf_equi_bai/NucTF/occup_profile/example2/O_good.mat;
 y1=Y{1}(:,1);
-load D:\Cell_protocol\NucRemod\occup_profile\O.mat;
+load /nuctf_equi_bai/NucRemod/occup_profile/O.mat;
 y2=occup_nuc{1}(:,1);
 clf;
 x=1:length(y1); 
