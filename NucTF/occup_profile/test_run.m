20db4c9ec34dc99bf4f82312f073d0442f0efde9
hungyok <punkienutts@gmail.com>
2022-08-07 10:38:58 +0200

    Update test_run.m

--- NucTF/occup_profile/test_run.m
+++ NucTF/occup_profile/test_run.m
@@ -1,4 +1,4 @@
 clear;
-load \NucTF\simplexM_tf30\output2\simplex_xval.txt;
+load /nuctf_equi_bai/NucTF/simplexM_tf30/output2/simplex_xval.txt;
 xfit=simplex_xval(:,1); TF=1; Eseq=1;
 [Y]=occupx(TF,Eseq,xfit);
