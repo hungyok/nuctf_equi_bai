0ea19a72bf5ddcbfd34c3fda1ec8ed2b7fc4cb0b
hungyok <punkienutts@gmail.com>
2022-09-03 21:34:14 +0200

    Update test_run.m

--- NucTF/occup_profile/test_run.m
+++ NucTF/occup_profile/test_run.m
@@ -1,4 +1,7 @@
 clear;
-load /nuctf_equi_bai/NucTF/simplexM_tf30/output2/simplex_xval.txt;
-xfit=simplex_xval(:,1); TF=1; Eseq=1;
-[Y]=occupx(TF,Eseq,xfit);
+path1 ='yourpath\nuc_energy\'; 
+path2 = 'yourpath\tf_energy_all\';    
+path3 ='yourpath\ndr_call\';
+load /NucTF/simplexM_top30/output2/simplex_xval.txt;
+xfit = simplex_xval (:,1); TF = 1; Eseq = 1;
+[O] = occupx(TF,Eseq,xfit,tfx,path1,path2,path3);
