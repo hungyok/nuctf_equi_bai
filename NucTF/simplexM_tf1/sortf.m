b25744492654acdb25b7be88217314bb958b3bb0
hungyok <punkienutts@gmail.com>
2022-09-08 20:04:45 +0200

    Add files via upload

---
+++ NucTF/simplexM_tf1/sortf.m
@@ -0,0 +1,13 @@
+function [Fi,Fsig]=sortf(Fsig,n)
+Fi=1:n+1; Fi=Fi'; 
+i=1; 
+while i<(n+1)
+      for j=(i+1):(n+1)
+          if Fsig(i,1)>Fsig(j,1)
+             Fsm=Fsig(j,1); Fim=Fi(j,1);
+             Fsig(j,1)=Fsig(i,1); Fi(j,1)=Fi(i,1);
+             Fsig(i,1)=Fsm; Fi(i,1)=Fim;
+           end
+      end
+      i=i+1;
+end
